92a69034d1c64caab12f5b858c86c087cdd4fd83
Mercedes Iglesias <meriglmar@alum.us.es>
2021-06-05 19:29:32 +0200

    Añadida la clase PilotoResource

---
+++ src/main/java/aiss/api/consume/PilotoResource.java
@@ -0,0 +1,69 @@
+package aiss.api.consume;
+
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+
+
+public class PilotoResource {
+
+	private String uri = "https://api-aiss.ey.r.appspot.com/";
+	//private String uri = "http://localhost:8095/api/songs";
+
+	
+	public Collection<Piloto> getAll() {
+		ClientResource cr = null;
+		Piloto [] songs = null;
+		try {
+			cr = new ClientResource(uri);
+			songs = cr.get(Piloto[].class);
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving all songs: " + cr.getResponse().getStatus());
+			throw re;
+		}
+		
+		return Arrays.asList(songs);
+	}
+	
+
+	public Piloto getSong(String pilotoId) {
+		//TODO
+		return null;
+	}
+	
+
+	public Piloto addSong(Song piloto) {
+		// TODO
+		return null;
+
+	}
+	
+	public boolean updatePiloto(Piloto piloto) {
+		// TODO
+		return false;
+	}
+	
+
+	public boolean deletePiloto(String pilotoId) {
+		ClientResource cr = null;
+		boolean success = true;
+		try {
+			cr = new ClientResource(uri + "/" + pilotoId);
+			cr.setEntityBuffering(true);		// Needed for using RESTlet from JUnit tests
+			cr.delete();
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when deleting the piloto: " + cr.getResponse().getStatus());
+			success = false;
+			throw re;
+		}
+		
+		return success;
+		
+	}
+}
